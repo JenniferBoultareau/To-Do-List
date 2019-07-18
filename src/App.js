96f995deebdf008485f10b5a753046b9c36206bd
Jennifer Boultareau <jennifer.boultareau@gmail.com>
2019-07-18 16:55:33 +0200

    home with title and responsivity

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="background">
+        <p className="title">TO DO LIST</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
