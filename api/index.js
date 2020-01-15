8f7e53cc6b70b3b59d4df2d8f780c27a81a23708
Jennifer <Jennifer@MacBook-Air-de-Jennifer.local>
2020-01-15 15:54:01 +0100

    test

--- api/index.js
+++ api/index.js
@@ -8,7 +8,7 @@ const api = express();
 const database = new Datastore('database.db');
 database.loadDatabase();
 
-api.use(cors({ origin: 'https://jenniferboultareau.fr/projets/todolist/' }));
+api.use(cors('*'));
 api.use(bodyParser.json());
 api.use(bodyParser.urlencoded({extended: false}));
 
